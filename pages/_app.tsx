add3deba888ded37a6e5e27dda359f788ec847ce
Avishkar15 <avishkarrshinde@gmail.com>
2025-06-24 00:43:48 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import '../styles/globals.css';
 import 'antd/dist/antd.css';
 import { AppProps } from 'next/app';
+import '../styles/dark-theme.css'
+
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
     return <Component {...pageProps} />;
